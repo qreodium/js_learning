c5559d0e27e81ebe82d4d4ddd0bfd130ee991180
Vladimir Shilov <deluxewhite@yandex.ru>
2021-08-17 21:40:43 +0700

    Made a function for creating a website based on a template. Added the ability to add a logo.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 new Swiper('.swiper-container', {
 	loop: true,
 	navigation: {
@@ -21,3 +22,51 @@ menuButton.addEventListener('click', function () {
 	menuButton.classList.toggle('menu-button-active');
 	menu.classList.toggle('header-active');
 })
+*/
+
+const getElement = (tagName, classNames) => {
+	const element = document.createElement(tagName);
+	if (classNames) {
+		element.classList.add(...classNames);
+	}
+	return element;
+}
+
+const createHeader = (param) => {
+	const header = getElement('header');
+	const container = getElement('div', ['container']);
+	const wrapper = getElement('div', ['header'])
+
+	console.log(`Logo: ${param.header.logo}`);
+	if (param.header.logo) {
+		console.log("logo loading");
+		const logo = getElement('img', ['logo']);
+		logo.src = param.header.logo;
+		logo.alt = 'Логотип ' + param.title;
+		wrapper.append(logo);
+	}
+
+	header.append(container);
+	container.append(wrapper);
+
+	return header;
+}
+
+const movieConstructor = (selector, options) => {
+
+	const app = document.querySelector(selector);
+	app.classList.add('body-app');
+
+	if (options.header) {
+		app.append(createHeader(options));
+	}
+
+}
+
+movieConstructor('.app',{
+	title: 'Ведьмак',
+	header: {
+		logo: 'witcher/logo.png'
+
+	}
+});
